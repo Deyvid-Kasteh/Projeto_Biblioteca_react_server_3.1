a7dc7ca92d944654bb80c086646d0a0e92c3f4e8
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-05 21:35:04 -0300

    add index file

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { Router } from "express";
+
+const app = express();
+
+const route = Router();
+
+route.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello World! Sucesso!",
+  });
+});
+
+app.use(route);
+
+app.listen(4001, () => {
+  console.log("listening on port 4001");
+});
