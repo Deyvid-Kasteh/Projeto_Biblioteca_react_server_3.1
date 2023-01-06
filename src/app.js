733de4bb57eb0bebf59bf14204ce4d06c92493ba
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-06 01:06:23 -0300

    cors ahead

--- src/app.js
+++ src/app.js
@@ -6,14 +6,15 @@ import './database/index.js';
 
 class App {
     constructor() {
+
         this.server = express();
         this.middleware();
         this.routes();
     }
 
     middleware() {
+        this.server.use(cors());
         this.server.use(express.json());
-        // this.server.use(cors());
     }
 
     routes() {
