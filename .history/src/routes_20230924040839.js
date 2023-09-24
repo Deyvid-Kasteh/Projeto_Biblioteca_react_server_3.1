5aa20c1e819e88fff59c16ad7aeaecc9c1d5e2d4
Deyvid Kasteh <kasteh21@gmail.com>
2023-09-24 04:27:13 -0300

    changeCheckboxState updated part 2

---
+++ .history/src/routes_20230924040839.js
@@ -0,0 +1,86 @@
+import { Router } from "express";
+import auth from "./middlewares/auth.js";
+import SessionsController from "./controllers/SessionsController.js";
+import HelloController from "./controllers/HelloController.js";
+import UsersController from "./controllers/UsersController.js";
+
+const routes = new Router();
+
+// --- Rota pública
+routes.get("/hello", HelloController.index);
+routes.post("/sessions", SessionsController.create);
+routes.post("/users", UsersController.create);
+routes.get("/Perfil/:id", UsersController.showUser);
+
+routes.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello World! Sucesso! Deyvid é o cara",
+  });
+});
+
+// --- middleware
+routes.use(auth);
+
+// --- Rosta protegida
+
+routes.patch("/Perfil/:id", UsersController.updateOne);
+routes.patch("/Perfil/:id/pic", UsersController.updatePic);
+routes.patch(
+  "/Perfil/:idUsuario/addBookToFavorites/:idLivro",
+  UsersController.addBookToFavorites
+);
+routes.patch(
+  "/Perfil/:idUsuario/addBookToSeeLater/:idLivro",
+  UsersController.addBookToSeeLater
+);
+
+routes.patch(
+  "/Perfil/:idUsuario/addBookToShoppingCart/:idLivro",
+  UsersController.addBookToShoppingCart
+);
+
+
+routes.patch(
+  "/Perfil/:idUsuario/changeCheckboxState/:idLivro",
+  UsersController.changeCheckboxState
+);
+
+
+changeCheckboxState;
+
+
+
+
+
+
+
+
+
+
+routes.delete(
+  "/Perfil/:idUsuario/destroyBookfromFavorites/:idLivro",
+  UsersController.destroyBookfromFavorites
+);
+
+routes.delete(
+  "/Perfil/:idUsuario/destroySeeLaterBook/:idLivro",
+  UsersController.destroySeeLaterBook
+);
+
+routes.delete(
+  "/Perfil/:idUsuario/destroyBookFromShoppingCart/:idLivro",
+  UsersController.destroyBookFromShoppingCart
+);
+
+// destroySeeLaterBook;
+
+// routes.get("/users", UsersController.index);
+// routes.get("/users/:id", UsersController.show);
+// routes.put("/users/:id", UsersController.update);
+// routes.delete("/users/:id", UsersController.destroy);
+// routes.get("/users/:user_id/repositories", RepositoriesController.index);
+// routes.post("/users/:user_id/repositories", RepositoriesController.create);
+// routes.delete("/users/:user_id/repositories", RepositoriesController.destroy);
+
+export default routes;
